507ec831aa4ed75e14f0f90fe9b673474e73e014
Kuba Florczuk <me@kuba.rocks>
2022-08-09 09:53:26 +0200

    add testimonials styling

--- src/components/home/TestimonialsList.tsx
+++ src/components/home/TestimonialsList.tsx
@@ -1,8 +1,24 @@
 import { inferQueryOutput, trpc } from "@app/utils/trpc";
 import { TestimonialItem } from "@app/components/home/TestimonialItem";
+import styled from "styled-components";
+import { SectionTitle } from "@app/components/common/SectionTitle";
 
 type TestimonialsList = inferQueryOutput<"testimonials.getAll">;
 export type Testimonial = TestimonialsList[number];
+
+const TestimonialsGrid = styled.div`
+  margin-top: 6rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 6rem;
+  @media (max-width: 920px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const TestimonialsSection = styled.section`
+  margin: 2rem 0;
+`;
 
 export const TestimonialsList = () => {
   const { data, isLoading } = trpc.useQuery(["testimonials.getAll"]);
@@ -10,11 +26,13 @@ export const TestimonialsList = () => {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <section>
-      <h3>Testimonials</h3>
-      {data?.map((testimonial) => (
-        <TestimonialItem key={testimonial.id} {...testimonial} />
-      ))}
-    </section>
+    <TestimonialsSection>
+      <SectionTitle>Testimonials</SectionTitle>
+      <TestimonialsGrid>
+        {data?.map((testimonial) => (
+          <TestimonialItem key={testimonial.id} {...testimonial} />
+        ))}
+      </TestimonialsGrid>
+    </TestimonialsSection>
   );
 };
