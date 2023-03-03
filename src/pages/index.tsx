e17901571996dbfa9ac8c52e47021e35fcfb7d43
Kuba Florczuk <me@kuba.rocks>
2023-03-03 19:11:35 +0100

    update daughter age

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
         content={`I'm a Full-Stack Developer and Team Leader based in Warsaw,
           Poland, with ${yearsOfExperience} years of commercial experience in Web
           Development and Team Management. Also a husband and father of one
-          sweet two-year-old girl. Huge fan of basketball and comics.`}
+          sweet four-year-old girl. Huge fan of basketball and comics.`}
       />
       <Technologies />
       <Testimonials testimonials={testimonials} />
